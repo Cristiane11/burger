7cefd7363b4c6adcf1fbc8db547f2d6fb56ee90b
Cristiane Abreu <bjwmcd@yahoo.com>
2018-05-27 12:24:00 -0400

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Meat');
+INSERT INTO burgers (burger_name) VALUES ('chicken');
+INSERT INTO burgers (burger_name, devoured) VALUES ('ham', true);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Tuna', true);
